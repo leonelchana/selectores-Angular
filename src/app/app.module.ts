44cb1c0a3e9177087aab41989d8da77d585cedeb
sistemasfinorleonel <96667421+sistemasfinorleonel@users.noreply.github.com>
2023-01-24 11:54:51 -0400

    final seccion 19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaisesRoutingModule } from './paises/paises-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PaisesRoutingModule,
+    
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/paises/paises.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { PaisesRoutingModule } from './paises-routing.module';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+ 
+
+@NgModule({
+  declarations: [
+    SelectorPageComponent
+  ],
+  imports: [
+    CommonModule,
+    PaisesRoutingModule,
+   
+    ReactiveFormsModule,
+  ],providers:[
+ 
+    
+
+  ]
+})
+export class PaisesModule { }
